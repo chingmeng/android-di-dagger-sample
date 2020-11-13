0c7adf2d40efe8e6701df54f6e3228f7d77bb442
chingmeng <sum2infinity@gmail.com>
2020-11-13 13:47:05 +0800

    Update app to inject dependencies to registration fragment
    - Update UserManager scope to Singleton

--- app/src/main/java/com/example/android/dagger/di/AppComponent.kt
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -1,11 +1,16 @@
 package com.example.android.dagger.di
 
 import android.content.Context
+import com.example.android.dagger.main.MainActivity
 import com.example.android.dagger.registration.RegistrationActivity
+import com.example.android.dagger.registration.enterdetails.EnterDetailsFragment
+import com.example.android.dagger.registration.termsandconditions.TermsAndConditionsFragment
 import dagger.BindsInstance
 import dagger.Component
+import javax.inject.Singleton
 
 // Definition of a Dagger component that adds info from the StorageModule to the graph
+@Singleton
 @Component(modules = [StorageModule::class])
 interface AppComponent {
 
@@ -19,4 +24,7 @@ interface AppComponent {
 
     // Define the classes that can be injected by this Component
     fun inject(activity: RegistrationActivity)
+    fun inject(fragment: EnterDetailsFragment)
+    fun inject(fragment: TermsAndConditionsFragment)
+    fun inject(activity: MainActivity)
 }
