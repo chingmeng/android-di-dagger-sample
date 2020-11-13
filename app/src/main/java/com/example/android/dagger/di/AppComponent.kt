6afebf1b236bdd26a2c82dc8dfed78e579255f86
chingmeng <sum2infinity@gmail.com>
2020-11-13 13:15:49 +0800

    Update AppComponent.kt with StorageModule
    - Add Storage module and provide SharedPreferencesStorage as Storage

--- app/src/main/java/com/example/android/dagger/di/AppComponent.kt
+++ app/src/main/java/com/example/android/dagger/di/AppComponent.kt
@@ -5,9 +5,17 @@ import com.example.android.dagger.registration.RegistrationActivity
 import dagger.BindsInstance
 import dagger.Component
 
-// Definition of a Dagger component
-@Component
+// Definition of a Dagger component that adds info from the StorageModule to the graph
+@Component(modules = [StorageModule::class])
 interface AppComponent {
+
+    // Factory to create instances of the AppComponent
+    @Component.Factory
+    interface Factory {
+        // With @BindsInstance, the Context passed in will be available in the graph
+        fun create(@BindsInstance context: Context): AppComponent
+    }
+
 
     // Define the classes that can be injected by this Component
     fun inject(activity: RegistrationActivity)
