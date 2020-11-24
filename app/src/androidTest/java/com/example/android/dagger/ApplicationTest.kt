c4fa3788aff1cd27bc024a0abf5af4657f667775
chingmeng <sum2infinity@gmail.com>
2020-11-24 22:49:54 +0800

    Update ApplicationTest.kt to include SplashActivity

--- app/src/androidTest/java/com/example/android/dagger/ApplicationTest.kt
+++ app/src/androidTest/java/com/example/android/dagger/ApplicationTest.kt
@@ -21,14 +21,18 @@ import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
-import com.example.android.dagger.main.MainActivity
+import com.example.android.dagger.splash.SplashActivity
 import org.junit.Test
+import java.util.concurrent.CountDownLatch
+import java.util.concurrent.TimeUnit
 
 class ApplicationTest {
 
     @Test
     fun runApp() {
-        ActivityScenario.launch(MainActivity::class.java)
+        ActivityScenario.launch(SplashActivity::class.java)
+        val latch = CountDownLatch(1)
+        latch.await(1, TimeUnit.SECONDS)
 
         // Should be in Registration/EnterDetails because the user is not registered
         onView(withText("Register to Dagger World!")).check(matches(isDisplayed()))
